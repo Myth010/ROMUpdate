6caba959cb145c9d60eae2a52a27841f7dd37679
Myth010 <myth@iohome.com>
2013-01-16 00:15:16 +0200

    Populated context menu that has working classes. Just need to modify the classes to fulfill their actual utility

--- src/com/myth/romupdater/MainActivity.java
+++ src/com/myth/romupdater/MainActivity.java
@@ -2,12 +2,16 @@ package com.myth.romupdater;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.PopupMenu;
+import android.widget.PopupMenu.OnMenuItemClickListener;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements OnMenuItemClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +19,32 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	
+	 public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
-	}
+	} 
 	public void pathPopup(View view) {
 	    PopupMenu popup = new PopupMenu(this, view);
 	    MenuInflater inflater = popup.getMenuInflater();
-	    inflater.inflate(R.menu.activity_main, popup.getMenu());
+	    inflater.inflate(R.menu.actions, popup.getMenu());
+	    popup.setOnMenuItemClickListener(this);
 	    popup.show();
+	}
+	@Override
+	public boolean onMenuItemClick(MenuItem item) {
+	    switch (item.getItemId()) {
+	        case R.id.path1:
+	            Intent action1 = new Intent(this, path1.class);
+	            startActivity(action1);
+	            return true;
+	        case R.id.path2:
+	            Intent action2 = new Intent(this, path2.class);
+	            startActivity(action2);
+	            return true;
+	        default:
+	            return false;
+	    }
 	}
 }
---
+++ src/com/myth/romupdater/path2.java
@@ -0,0 +1,13 @@
+package com.myth.romupdater;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class path2 extends Activity	{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState)	{
+					super.onCreate(savedInstanceState);
+					setContentView(R.layout.activity_main);
+	}
+}
